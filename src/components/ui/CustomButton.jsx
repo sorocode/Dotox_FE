6a4b7f7294394069f9391fe28b005ff817ff08ca
sorocode <tudgus010707@naver.com>
2024-07-11 12:21:04 +0900

    ✨ 커스텀 버튼 컴포넌트추가

---
+++ src/components/ui/CustomButton.jsx
@@ -0,0 +1,22 @@
+import { Button } from "./button";
+
+const CustomButton = ({ fontSize, filled, children }) => {
+  const sizeClass =
+    fontSize === "large"
+      ? "py-3 px-6 text-lg"
+      : fontSize === "small"
+      ? "py-1 px-3 text-sm"
+      : "py-2 px-4 text-base";
+
+  // Define filled-based classes with main color
+  const filledClass = filled
+    ? "bg-mintGreen border-transparent text-white hover:bg-mintGreen"
+    : "bg-transparent border-mintGreen border-2 text-mintGreen hover:bg-white";
+
+  // Combine classes
+  const cssClass = `flex justify-center items-center border-[1px] w-full my-2 ${sizeClass} ${filledClass} rounded-lg hover:opacity-80 `;
+
+  return <Button className={cssClass}>{children}</Button>;
+};
+
+export default CustomButton;
