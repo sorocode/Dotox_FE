c817d8a3b7102f5ae52dd7c85c5a2822aef0f7bd
sorocode <tudgus010707@naver.com>
2024-07-11 22:10:39 +0900

    💄 헤더 색상 변경

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -3,7 +3,7 @@ import { AccountCircle } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 const Header = () => {
   return (
-    <div className="flex justify-between items-center bg-mintGreen px-6 py-2">
+    <div className="flex justify-between items-center bg-deepGreen px-6 py-2">
       <Link to="/" className="flex items-center ">
         <img src="mainLogo.png" alt="logo" width={50} />
         {/* <span className="text-white text-xl font-semibold font-ShadowsIntoLight">
