8413ed4f521eeabf5204d8ba967662e42460bf2e
sorocode <tudgus010707@naver.com>
2024-07-11 20:25:50 +0900

    ✨ 페이지 상단부 완성

--- src/components/pages/RecommendPage.jsx
+++ src/components/pages/RecommendPage.jsx
@@ -1,5 +1,24 @@
+import FriendImage from "@/assets/recommend1.png";
+import ClubImage from "@/assets/recommend2.png";
+import { Link } from "react-router-dom";
+
 const RecommendPage = () => {
-  return <p>__님의 취미 친구를 저희가 만들어 드릴게요!</p>;
+  return (
+    <>
+      <div className="flex flex-col font-bold text-lg">
+        ___님의 취미 친구를 <span>저희가 만들어 드릴게요!😝</span>
+      </div>
+      <div className="flex justify-center gap-2 my-3">
+        {/* TODO:추후 상세 페이지 링크 추가하기 */}
+        <Link>
+          <img src={FriendImage} alt="Friend Image" />
+        </Link>
+        <Link>
+          <img src={ClubImage} alt="Club Image" />
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default RecommendPage;
