505d6fd972bfd06fb53e9405b7fb591e29be19b5
sorocode <tudgus010707@naver.com>
2024-07-11 22:09:41 +0900

    💄 헤더에 메인로고 추가

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="flex justify-between items-center bg-mintGreen px-6 py-2">
-      <Link to="/" className="flex items-center gap-2">
-        {/* <img src="/logo.png" alt="logo" width={50} /> */}
-        <span className="text-white text-xl font-semibold font-ShadowsIntoLight">
+      <Link to="/" className="flex items-center ">
+        <img src="mainLogo.png" alt="logo" width={50} />
+        {/* <span className="text-white text-xl font-semibold font-ShadowsIntoLight">
           Dotox
-        </span>
+        </span> */}
       </Link>
 
       <Link to="/login">
