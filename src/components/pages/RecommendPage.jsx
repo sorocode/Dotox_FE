9ed71df2f18f50963d72afa4cd227290d123efdc
sorocode <tudgus010707@naver.com>
2024-07-11 21:36:33 +0900

    ✨ Interest Area 추가

--- src/components/pages/RecommendPage.jsx
+++ src/components/pages/RecommendPage.jsx
@@ -1,6 +1,8 @@
 import FriendImage from "@/assets/recommend1.png";
 import ClubImage from "@/assets/recommend2.png";
 import { Link } from "react-router-dom";
+import InterestsArea from "../ui/Recommend/InterestsArea";
+import CustomButton from "../ui/CustomButton";
 
 const RecommendPage = () => {
   return (
@@ -8,7 +10,7 @@ const RecommendPage = () => {
       <div className="flex flex-col font-bold text-lg">
         ___님의 취미 친구를 <span>저희가 만들어 드릴게요!😝</span>
       </div>
-      <div className="flex justify-center gap-2 my-3">
+      <div className="flex justify-evenly gap-2 my-3">
         {/* TODO:추후 상세 페이지 링크 추가하기 */}
         <Link>
           <img src={FriendImage} alt="Friend Image" />
@@ -16,6 +18,11 @@ const RecommendPage = () => {
         <Link>
           <img src={ClubImage} alt="Club Image" />
         </Link>
+      </div>
+      <InterestsArea />
+      <div className="mt-6">
+        <CustomButton>취미 추천 받으러 가기</CustomButton>
+        <CustomButton filled>퀘스트 보러 가기</CustomButton>
       </div>
     </>
   );
---
+++ src/components/ui/Recommend/InterestsArea.jsx
@@ -0,0 +1,24 @@
+import InterestItem from "./InterestItem";
+import SelectContainer from "./SelectContainer";
+
+const InterestsArea = () => {
+  return (
+    <div className="mt-4">
+      <div className="flex flex-col my-3 font-bold text-lg gap-1">
+        <span className="flex items-center gap-2">
+          <SelectContainer />
+          사람들은
+        </span>
+        <span>
+          지금 <span className="text-lightGreen">여기</span>에 관심이 있어요
+        </span>
+      </div>
+      <InterestItem rank={1} title="독서" interestValue={80} />
+      <InterestItem rank={2} title="필라테스" interestValue={67} />
+      <InterestItem rank={3} title="향수공방" interestValue={42} />
+      <InterestItem rank={4} title="반지공방" interestValue={30} />
+    </div>
+  );
+};
+
+export default InterestsArea;
