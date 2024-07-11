d4be96f39f7d6c7fe97cb27dfc3a2f8ce7b9b99a
sorocode <tudgus010707@naver.com>
2024-07-11 21:50:22 +0900

    chore:할일 주석 추가

--- src/components/ui/Recommend/InterestsArea.jsx
+++ src/components/ui/Recommend/InterestsArea.jsx
@@ -13,6 +13,7 @@ const InterestsArea = () => {
           지금 <span className="text-lightGreen">여기</span>에 관심이 있어요
         </span>
       </div>
+      {/* TODO: 마커나 레이블 어떤식으로 할지 고민해보기 */}
       <InterestItem rank={1} title="독서" interestValue={80} />
       <InterestItem rank={2} title="필라테스" interestValue={67} />
       <InterestItem rank={3} title="향수공방" interestValue={42} />
