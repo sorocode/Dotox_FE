ea02f40dd3144150695c5873e41e5b99d7885627
sorocode <tudgus010707@naver.com>
2024-07-13 21:38:30 +0900

    chore: update CardContainer layout to use max-w-2xl class

--- src/components/ui/Store/CardContainer.jsx
+++ src/components/ui/Store/CardContainer.jsx
@@ -4,7 +4,7 @@ import CardItem from "./CardItem";
 const CardContainer = () => {
   //  FIXME:하드 코딩된 값 변경하기
   return (
-    <Carousel className="w-full max-w-sm">
+    <Carousel className="w-full max-w-2xl">
       <CarouselContent>
         <CardItem />
         <CardItem />
