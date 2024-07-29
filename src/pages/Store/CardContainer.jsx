e259cc91f169e479365a2293b7608cc572141675
sorocode <tudgus010707@naver.com>
2024-07-29 21:09:03 +0900

    ✨ 카드아이템 props 추가

--- src/pages/Store/CardContainer.jsx
+++ src/pages/Store/CardContainer.jsx
@@ -1,14 +1,41 @@
 import { Carousel, CarouselContent } from "../../components/ui/carousel.jsx";
 import CardItem from "./CardItem.jsx";
 
-const CardContainer = () => {
+const CardContainer = ({ data }) => {
   //  FIXME:하드 코딩된 값 변경하기
   return (
     <Carousel className="w-full max-w-2xl">
       <CarouselContent>
-        <CardItem />
-        <CardItem />
-        <CardItem />
+        <CardItem
+          category="핸드메이드"
+          region="대구/경북"
+          previousPrice={50000}
+          discountRate={0.1}
+          title="나만의 실버링 만들기"
+          heart={932}
+          stars={4.7}
+          imageUrl="ring.png"
+        />
+        <CardItem
+          category="핸드메이드"
+          region="부산/경남"
+          previousPrice={50000}
+          discountRate={0.1}
+          heart={932}
+          stars={4.7}
+          title="나만의 머핀 만들기"
+          imageUrl="muffin.png"
+        />
+        <CardItem
+          category="핸드메이드"
+          region="대구/경북"
+          previousPrice={50000}
+          discountRate={0.1}
+          heart={932}
+          stars={4.7}
+          title="나만의 꽃 만들기"
+          imageUrl="flower.png"
+        />
       </CarouselContent>
     </Carousel>
   );
--- src/pages/Store/CardItem.jsx
+++ src/pages/Store/CardItem.jsx
@@ -1,46 +1,61 @@
 import { Badge } from "../../components/ui/badge.jsx";
 import { Card, CardContent } from "../../components/ui/card.jsx";
-import ClassImage from "@/assets/fakeImages/classImage.png";
+
 import HeartIcon from "@/assets/heart.svg";
 import StarIcon from "@/assets/star.svg";
 import { CarouselItem } from "../../components/ui/carousel.jsx";
 
-const CardItem = () => {
+const CardItem = ({
+  category,
+  stars,
+  heart,
+  region,
+  imageUrl,
+  title,
+  previousPrice,
+  discountRate,
+}) => {
   //  FIXME:하드 코딩된 값 변경하기
   return (
     <CarouselItem className="basis-1/2 md:basis-1/3 text-[0.6rem]">
       <Card className="pt-4 flex flex-col items-center justify-center">
         <CardContent className="flex flex-col gap-1">
-          <img src={ClassImage} alt="Class Image" className="rounded-xl" />
+          <img src={imageUrl} alt="Class Image" className="rounded-xl" />
           <div className="flex justify-between items-center">
             <Badge className="bg-mintGreen">
               <span className="whitespace-nowrap text-[0.6rem]">
-                핸드메이드
+                {category}
               </span>
             </Badge>
             <div className="flex gap-2 justify-end">
               <div className="flex items-center">
                 <img src={StarIcon} alt="Star Icon" />
-                <span className="">4.7</span>
+                <span className="">{stars}</span>
               </div>
               <div className="flex items-center">
                 <img src={HeartIcon} alt="Heart Icon" />
-                <span>629</span>
+                <span>{heart}</span>
               </div>
             </div>
           </div>
 
           <div className="productTitle ">
             <span className="flex justify-end font-semibold text-right text-xs">
-              [대구/경북]나만의 실버링 만들기
+              [{region}]{title}
             </span>
           </div>
 
           <div className="flex gap-1 justify-end">
-            <span className="text-red-600 font-semibold ">10%</span>
-            <span className="previousPrice line-through">₩50,000</span>
+            <span className="text-red-600 font-semibold ">
+              {discountRate * 100}%
+            </span>
+            <span className="previousPrice line-through">
+              ₩{previousPrice?.toLocaleString()}
+            </span>
           </div>
-          <div className="flex justify-end font-bold text-sm">₩45.000</div>
+          <div className="flex justify-end font-bold text-sm">
+            ₩{(previousPrice * (1 - discountRate))?.toLocaleString()}
+          </div>
         </CardContent>
       </Card>
     </CarouselItem>
