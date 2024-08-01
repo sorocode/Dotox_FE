9edd8e16b6ab83a505772ea2176b7779bbf3b84e
sorocode <tudgus010707@naver.com>
2024-08-01 15:43:20 +0900

    🎨 상태에 다른 데이터도 추가

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,34 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 
+const initialState = {
+  isLogin: false,
+  userID: undefined,
+  name: undefined,
+  gender: undefined,
+  point: undefined,
+  time: undefined,
+  hobby: undefined,
+  email: undefined,
+};
 export const useLoginStore = create(
   devtools((set) => ({
-    isLogin: false,
-    userID: undefined,
-    name: undefined,
+    // isLogin: false,
+    // userID: undefined,
+    // name: undefined,
+    initialState,
     setLogin: (userData) =>
-      set({ isLogin: true, userID: userData.userID, name: userData.name }),
-    setLogout: () =>
-      set({ isLogin: false, userID: undefined, name: undefined }),
+      set({
+        ...initialState,
+        isLogin: true,
+        userID: userData.userID,
+        name: userData.name,
+        gender: userData.gender,
+        point: userData.point,
+        hobby: userData.hobby,
+        age: userData.age,
+        email: userData.email,
+      }),
+    setLogout: () => set({ ...initialState }),
   }))
 );
