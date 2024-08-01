f16796af44e315536c8f8618a96b7ba4a81a4114
sorocode <tudgus010707@naver.com>
2024-08-01 15:28:16 +0900

    🎨 로그인 시 이름까지 상태관리에 넣기

--- src/store/store.js
+++ src/store/store.js
@@ -5,7 +5,10 @@ export const useLoginStore = create(
   devtools((set) => ({
     isLogin: false,
     userID: undefined,
-    setLogin: (userID) => set({ isLogin: true, userID: userID }),
-    setLogout: () => set({ isLogin: false, userID: undefined }),
+    name: undefined,
+    setLogin: (userData) =>
+      set({ isLogin: true, userID: userData.userID, name: userData.name }),
+    setLogout: () =>
+      set({ isLogin: false, userID: undefined, name: undefined }),
   }))
 );
