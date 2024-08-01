d133b2712c6f24c976e53647dcdb2730ee49343c
sorocode <tudgus010707@naver.com>
2024-08-01 15:52:36 +0900

    ✨ 로그인된 계정 이름으로 뜨게하기

--- src/pages/Recommend/RecommendPage.jsx
+++ src/pages/Recommend/RecommendPage.jsx
@@ -4,13 +4,17 @@ import { Link } from "react-router-dom";
 import InterestsArea from "@/pages/Recommend/InterestsArea.jsx";
 import CustomButton from "../../components/ui/CustomButton.jsx";
 import useCheckLogin from "@/hooks/useCheckLogin.jsx";
+import { useLoginStore } from "@/store/store.js";
 
 const RecommendPage = () => {
   useCheckLogin();
+
+  // 전역 상태에서 유저 정보 불러오기
+  const { name } = useLoginStore();
   return (
     <>
       <div className="flex flex-col font-bold text-lg">
-        ___님의 취미 친구를 <span>저희가 만들어 드릴게요!😝</span>
+        {name}님의 취미 친구를 <span>저희가 만들어 드릴게요!😝</span>
       </div>
       <div className="flex justify-evenly gap-2 my-3">
         {/* TODO:추후 상세 페이지 링크 추가하기 */}
