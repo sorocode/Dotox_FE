d042b86af35bfe6f388b1157a1e25fb3630929e3
sorocode <tudgus010707@naver.com>
2024-08-02 20:41:14 +0900

    🌱 로딩 페이지 껍데기

---
+++ src/pages/Login/Loading.jsx
@@ -0,0 +1,12 @@
+import { Spinner } from "@chakra-ui/react";
+
+const LoadingPage = () => {
+  return (
+    <>
+      <div>로딩중</div>
+      <Spinner />
+    </>
+  );
+};
+
+export default LoadingPage;
