6a71719347548d99a1e4f121d108ebbc1973cc15
sorocode <tudgus010707@naver.com>
2024-08-03 15:01:57 +0900

    ✨ 소셜로그인 인가코드 axios 요청

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,11 +2,27 @@ import { QueryClient } from "@tanstack/react-query";
 import { apiRequester } from "./axiosConfig";
 
 export const queryClient = new QueryClient();
+const SERVER_URL = import.meta.env.VITE_API_URL;
 
-// 로그인
-export const login = async (userID) => {
-  const req = await apiRequester.post("api/login", userID);
-  return req.data;
+// // 네이버 로그인
+// export const naverLogin = async () => {
+//   const req = await apiRequester.get(
+//     SERVER_URL + "/oauth2/authorization/naver"
+//   );
+//   return req.data;
+// };
+// // 구글 로그인
+// export const googleLogin = async () => {
+//   const req = await apiRequester.get(
+//     SERVER_URL + "/oauth2/authorization/google"
+//   );
+//   return req.data;
+// };
+
+//소셜 로그인 시 인가 코드 전달
+export const sendCode = async (data) => {
+  const response = await apiRequester.post(SERVER_URL + "/oauth2/token", data);
+  return response.data;
 };
 
 // 로그인한 유저 정보 가져오기
