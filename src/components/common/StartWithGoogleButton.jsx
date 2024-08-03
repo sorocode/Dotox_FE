fa579d5a206cd911201ab695b84709fe9f323c09
sorocode <tudgus010707@naver.com>
2024-08-03 15:02:55 +0900

    🐛 ...props 추가

--- src/components/common/StartWithGoogleButton.jsx
+++ src/components/common/StartWithGoogleButton.jsx
@@ -1,8 +1,8 @@
 import { Button } from "../ui/button";
 
-const StartWithGoogleButton = () => {
+const StartWithGoogleButton = ({ ...props }) => {
   return (
-    <Button variant="ghost">
+    <Button variant="ghost" {...props}>
       <div className="flex bg-[#F2F2F2] items-center justify-center gap-2 rounded-md w-full">
         <img
           src="SignGoogle.png"
