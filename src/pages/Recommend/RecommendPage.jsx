86e9542597a42ae14397e0be5197de4ab8e3eabb
박지현 <jihyoun2002@knu.ac.kr>
2024-08-06 01:40:34 +0900

    Update RecommendPage.jsx

--- src/pages/Recommend/RecommendPage.jsx
+++ src/pages/Recommend/RecommendPage.jsx
@@ -5,7 +5,6 @@ import InterestsArea from "@/pages/Recommend/InterestsArea.jsx";
 import CustomButton from "../../components/ui/CustomButton.jsx";
 import useCheckLogin from "@/hooks/useCheckLogin.jsx";
 import { useLoginStore } from "@/store/store.js";
-import React from "react";
 
 const RecommendPage = () => {
   useCheckLogin();
@@ -18,10 +17,11 @@ const RecommendPage = () => {
         {name}님의 취미 친구를 <span>저희가 만들어 드릴게요!😝</span>
       </div>
       <div className="flex justify-evenly gap-2 my-3">
-        <Link to="/find-friend">
+        
+        <Link>
           <img src={FriendImage} alt="Friend Image" />
         </Link>
-        <Link > 
+        <Link>
           <img src={ClubImage} alt="Club Image" />
         </Link>
       </div>
