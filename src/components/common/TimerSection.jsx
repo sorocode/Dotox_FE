7639a86b16c68535f835d700e66cfb9b103d36ae
sorocode <tudgus010707@naver.com>
2024-08-06 20:07:32 +0900

    ✨ 스크린타임 백으로 전송

--- src/components/common/TimerSection.jsx
+++ src/components/common/TimerSection.jsx
@@ -1,16 +1,25 @@
-import { getElapsedTime, startTimer } from "@/utils/timer";
+import { getElapsedTime, startTimer, stopTimer } from "@/utils/timer";
+
 import { useEffect, useState } from "react";
+import { Button } from "../ui/button";
+import { updateUserInfo } from "@/utils/http";
+import { useLoginStore } from "@/store/store";
 
 const TimerSection = () => {
   const [elapsedTime, setElapsedTime] = useState(0);
-
+  const { userID } = useLoginStore();
   useEffect(() => {
-    const interval = setInterval(() => {
+    const interval = setInterval(async () => {
       setElapsedTime(getElapsedTime());
+      try {
+        await updateUserInfo(userID, { screenTime: elapsedTime });
+      } catch (err) {
+        throw new Error(err);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [elapsedTime, userID]);
 
   const handleStart = () => {
     startTimer();
@@ -24,12 +33,16 @@ const TimerSection = () => {
     return `${minutes}m ${seconds}s`;
   };
   return (
-    <>
-      <button onClick={handleStart}>타이머시작</button>
+    <div className="flex flex-col justify-center items-center">
+      <Button onClick={handleStart} variant="ghost">
+        <img src="mainLogo.png" width="15%" />
+        <span>다른 앱 시작</span>
+      </Button>
       <p className="text-lg font-bold">
-        현재 도톡스 시간: {formatTime(elapsedTime)}
+        다른 앱 사용시간: {formatTime(elapsedTime)}
       </p>
-    </>
+      <Button onClick={stopTimer}>중지</Button>
+    </div>
   );
 };
 
